8ed053ccaa61ab365fbfcaeae86aef58e29b7b55
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2022-01-04 19:53:49 +1030

    Linking with postgres

--- app/main.py
+++ app/main.py
@@ -1,27 +1,45 @@
+"""[summary]
+
+    Raises:
+        HTTPException: [description]
+        HTTPException: [description]
+        HTTPException: [description]
+
+    Returns:
+        [type]: [description]
+    """
 import logging
-from random import randrange
+import time
+# from random import randrange
 
 import psycopg2
 from fastapi import FastAPI, HTTPException, Response, status
 from psycopg2.extras import RealDictCursor
+from psycopg2 import DatabaseError, Error
 
-import app.helper_functions as helper_func
-from app.data.test_data import posts
+# import app.helper_functions as helper_func
+# from app.data.test_data import posts
 from app.models.post import Post
 
 logging.basicConfig(filename='fastapi.log',
                     level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 app = FastAPI()
-try:
-    conn = psycopg2.connect(host='localhost',
-                            database='fastapi',
-                            user='fastapi',
-                            password='fastapi',
-                            cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-except psycopg2.Error as err:
-    print(f'DB connection error: {err}')
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',
+                                database='fastapi',
+                                user='fastapi',
+                                password='fastapi',
+                                cursor_factory=RealDictCursor)
+        # cursor = conn.cursor()
+        print('Database connection established')
+        break
+    except Error as err:
+        print('Connection failed!')
+        print(f'DB connection error: {err}')
+        print('Retrying....')
+        time.sleep(4)
 
 
 @app.get("/")
@@ -38,66 +56,114 @@ def read_root():
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 # or create_post(pay_load: dict = Body(...)):
 def create_post(post: Post):
-    '''
+    """
     create a new post
-    '''
-    print(f'Post(pydantic model) = {post}')
+    """
+
+    # print(f'Post(pydantic model) = {post}')
     print(f'Post(python dict) = {post.dict()}')
-    post.post_id = randrange(10, 10000)
+
+    post_dict = post.dict()
     # Save new post
-    posts.append(post)
+    qry = """
+    INSERT INTO posts ("title", "content", "published") VALUES (%s, %s, %s) returning *;
+    """
+    try:
+        with conn.cursor() as cur:
+            cur.execute(qry, (post_dict['title'], post_dict['content'],
+                        post_dict['published']))
+            # new_post = cur.fetchone()[0]
+            new_post = cur.fetchone()
+    except (Exception, DatabaseError) as error:
+        logging.error(error)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=error) from error
+
+    conn.commit()
+
     return {
         "message": "Post created successfully",
-        "post": post
+        "post": new_post
     }
 
 
 @app.get("/posts", status_code=status.HTTP_200_OK)
 def get_posts():
-    '''
+    """[summary]
       Retrieve all posts
-    '''
+    """
+    qry = """
+    SELECT * FROM posts
+    """
+    with conn.cursor() as cur:
+        cur.execute(qry)
+        posts = cur.fetchall()
     return {"data": posts}
 
 
 @app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
 def get_post(post_id: int):
-    '''
+    """
     Retrieve a specific post based on post_id
-    '''
-    post = helper_func.find_post(post_id, posts)
-    if not post:
+    """
+    qry = """
+    SELECT * FROM posts WHERE id=%s
+    """
+    try:
+        with conn.cursor() as cur:
+            cur.execute(qry, (post_id, ))
+            post = cur.fetchone()
+    except (Exception, DatabaseError) as error:
         logging.error('Get Post. post_id:%s does not exist!', post_id)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"DB Error: {error}") from error
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with post_id={post_id} was not found!")
     return post
 
 
-@app.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
+@ app.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
 def update_post(post_id: int, post: Post):
-    '''
+    """
     Update a post
-    '''
-    updated = helper_func.is_update_post(post_id, post.dict(), posts)
-    if not updated:
-        logging.error('Update Post. post_id:%s does not exist!', post_id)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Post with post_id: {post_id} does not exist!')
+    """
+
+    qry = """
+    UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s returning *;
+    """
+    post_dict = post.dict()
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                qry, (post_dict['title'], post_dict['content'], post_dict['published'], post_id))
+            updated_post = cur.fetchone()
+    except (Exception, DatabaseError) as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Error:{error}') from error
+
+    conn.commit()
 
     return {"message": "Post has been updated",
-            "post": helper_func.find_post(post_id, posts)
+            "post": updated_post
             }
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int):
-    '''
+    """
     Delete a post
-    '''
+    """
+    qry = """
+    DELETE FROM posts WHERE id=%s
+    """
+    try:
+        with conn.cursor() as cur:
+            cur.execute(qry, (post_id,))
+    except (Exception, DatabaseError) as error:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=error) from error
+    conn.commit()
 
-    deleted = helper_func.is_delete_post(post_id, posts)
-    if not deleted:
-        logging.error('Delete Post. post_id:%s does not exist!', post_id)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Post with post_id = {post_id} does not exist!')
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/models/post.py
+++ app/models/post.py
@@ -1,11 +1,21 @@
-from pydantic import BaseModel
-from typing import Optional
-'''
+"""
 A schema class to force clients to create valid posts
-'''
+    """
+from typing import Optional
+
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+from pydantic import BaseModel
+
+
 class Post(BaseModel):
+    """[summary]
+
+    Args:
+        BaseModel ([type]): [description]
+    """
     id: Optional[int] = None
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None    
+    rating: Optional[int] = None
